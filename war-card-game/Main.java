98c94296a0454915b8a90c39301aa59ac6efe879
Aaron Estaris <aaronestaris58@gmail.com>
2026-07-29 11:28:00 +0800

    Final Version Complete

--- war-card-game/Main.java
+++ war-card-game/Main.java
@@ -8,6 +8,9 @@ public class Main {
 
     public static void main(String[] args) {
         WarGame game = new WarGame();
+        System.out.println();
+        System.out.println("Welcome to the Aaron War Card Game. Good Luck and I Hope You Enjoy the Game!");
+        System.out.println();
         game.play();
     }
 }
